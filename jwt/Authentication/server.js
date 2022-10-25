ae69af10b9e1e56ab90dce8c43587127a366d7c2
peterdangvnngithub <peterdangvnn@gmail.com>
2022-10-25 11:35:32 +0700

    25/10/2022: Finish login method Authentication

--- jwt/Authentication/server.js
+++ jwt/Authentication/server.js
@@ -17,11 +17,35 @@ app.get('/users', (req, res) => {
 	res.json(users);
 });
 
-app.post('/users', (req, res) => {
-	const user = { name: req.body.name, password: req.body.password };
-	users.push(user);
-	//201 Created: The request has been fulfilled, resulting in the creation of a new resource
-	res.status(201).send();
+app.post('/users', async (req, res) => {
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+		const user = { name: req.body.name, password: hashedPassword };
+
+		users.push(user);
+
+		//201 Created: The request has been fulfilled, resulting in the creation of a new resource
+		res.status(201).send();
+	} catch (error) {
+		// 500 Internal Server Error: A generic error message
+		res.status(500).send();
+	}
+});
+
+app.post('/users/login', async (req, res) => {
+	const user = users.find((user) => user.name === req.body.name);
+	if (user === null) {
+		res.status(400).send('Cannot find user');
+	}
+
+	try {
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			res.send('Success');
+		} else {
+			res.send('Not Allowed');
+		}
+	} catch (error) {}
 });
 
 app.listen(3000);
